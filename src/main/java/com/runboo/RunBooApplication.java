cfcf754f79f880c0ee9b76aad4b5dc21b8aff7de
김동건 <mexaen@naver.com>
2025-12-11 23:26:28 +0900

    Let's Go

---
+++ src/main/java/com/runboo/RunBooApplication.java
@@ -0,0 +1,13 @@
+package com.runboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunBooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunBooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/runboo/RunBooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunBooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
