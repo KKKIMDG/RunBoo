047b4f95a259650c0dcc52ea84d8f374d2bdded6
i3inni <fkdl4862@a.ut.ac.kr>
2025-12-29 17:23:11 +0900

    로직 다시 생각

--- backend/src/main/java/com/runboo/domain/challenge/controller/UserChallengeController.java
+++ backend/src/main/java/com/runboo/domain/challenge/controller/UserChallengeController.java
@@ -1,25 +1,11 @@
 package com.runboo.domain.challenge.controller;
 
-import com.runboo.domain.challenge.dto.UserChallengeDto;
-import com.runboo.domain.challenge.service.UserChallengeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user-challenges")
 public class UserChallengeController {
 
-    private final UserChallengeService userChallengeService;
-
-    // 유저에게 해당하는 진행중인 도전과제 조회
-    @GetMapping("/ongoing/{userId}")
-    public List<UserChallengeDto> getOngoingChallenges(@PathVariable int userId) {
-        return userChallengeService.getOngoingChallenges(userId);
-    }
 }
