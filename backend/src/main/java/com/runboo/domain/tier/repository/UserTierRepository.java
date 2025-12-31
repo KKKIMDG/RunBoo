19fd09a9e215ffccb906ac336ecfb5bf3e59e576
i3inni <fkdl4862@a.ut.ac.kr>
2025-12-31 17:50:33 +0900

    삭제

--- backend/src/main/java/com/runboo/domain/tier/repository/UserTierRepository.java
+++
@@ -1,8 +0,0 @@
-package com.runboo.domain.tier.repository;
-
-import com.runboo.domain.tier.entity.UserTier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTierRepository extends JpaRepository<UserTier, Long> {
-
-}
