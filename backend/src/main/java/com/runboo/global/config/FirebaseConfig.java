26be2e88415daa4736d26a04495ccc7d74f12fd3
이차원 <ckdnjs7456@naver.com>
2026-01-08 09:21:58 +0900

    FCM 연동, 안드로이드 네이티브 빌드 완료. 일단 스케줄러로 1분 마다 알림오게 해놨음. backend/secrets/runboo-fcm.json 파일 넣으세요. fcm 키 파일임

--- backend/src/main/java/com/runboo/global/config/FirebaseConfig.java
+++ backend/src/main/java/com/runboo/global/config/FirebaseConfig.java
@@ -4,18 +4,25 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${google.application.credentials}")
+    private String credentialsPath;
+
     @PostConstruct
     public void init() throws IOException {
 
+        FileInputStream serviceAccount =
+                new FileInputStream(credentialsPath);
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
